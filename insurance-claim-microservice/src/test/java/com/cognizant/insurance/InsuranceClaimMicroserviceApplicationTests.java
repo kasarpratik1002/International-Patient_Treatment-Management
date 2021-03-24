492643b8393e87fb0fe3580ff8ea99e4c3f1bc8f
Sarvadnya <Sarvadnya@host.docker.internal>
2021-03-24 20:08:16 +0530

    Code Coverage 90%

--- insurance-claim-microservice/src/test/java/com/cognizant/insurance/InsuranceClaimMicroserviceApplicationTests.java
+++ insurance-claim-microservice/src/test/java/com/cognizant/insurance/InsuranceClaimMicroserviceApplicationTests.java
@@ -1,9 +1,7 @@
 package com.cognizant.insurance;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * 
@@ -15,7 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
  *
  */
 @SpringBootTest
-@RunWith(SpringRunner.class)
+
 class InsuranceClaimMicroserviceApplicationTests {
 
 	@Test
--- ip-treatment-offerings/src/test/java/com/cognizant/offerings/IpTreatmentOfferingsApplicationTests.java
+++ ip-treatment-offerings/src/test/java/com/cognizant/offerings/IpTreatmentOfferingsApplicationTests.java
@@ -1,9 +1,7 @@
 package com.cognizant.offerings;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * 
@@ -14,7 +12,6 @@ import org.springframework.test.context.junit4.SpringRunner;
  *         test
  */
 @SpringBootTest
-@RunWith(SpringRunner.class)
 public class IpTreatmentOfferingsApplicationTests {
 
 	@Test
