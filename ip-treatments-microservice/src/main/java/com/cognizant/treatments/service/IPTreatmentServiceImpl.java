07852b26df1e7673737de7135a6c0192f5a5f55a
Shubham Umate <72297995+megamindreb@users.noreply.github.com>
2021-03-31 16:21:13 +0530

    Final

--- ip-treatments-microservice/src/main/java/com/cognizant/treatments/service/IPTreatmentServiceImpl.java
+++ ip-treatments-microservice/src/main/java/com/cognizant/treatments/service/IPTreatmentServiceImpl.java
@@ -14,48 +14,42 @@ import com.cognizant.treatments.model.TreatmentPlan;
 import com.cognizant.treatments.repository.PatientDetailRepository;
 import com.cognizant.treatments.repository.TreatmentPlanRepository;
 
-import lombok.extern.slf4j.Slf4j;
-
 /**
  * 
  * @author Pratik, Shubham, Pratik, Kavya
  * 
- * 		service class that implements the interface IPTreatmentOfferingsService
- * 		that includes business logic method definitions
+ *         service class that implements the interface
+ *         IPTreatmentOfferingsService that includes business logic method
+ *         definitions
  *
  */
 
-@Slf4j
 @Service
 public class IPTreatmentServiceImpl implements IPTreatmentService {
 
-	
 	/**
 	 * autowires the AuthClient(feign client) to communicate with authorization
-	 * autowires PatientDetailDetailRepository and
-	 * TreatmentPlanRepository
+	 * autowires PatientDetailDetailRepository and TreatmentPlanRepository
 	 */
-	
+
 	@Autowired
 	AuthClient authClient;
-	
+
 	@Autowired
 	PatientDetailRepository patientDetailRepository;
 
 	@Autowired
 	TreatmentPlanRepository treatmentPlanRepository;
 
-	
 	/**
 	 * to return the treatment plan
+	 * 
 	 * @param patientDetail,cost
 	 * @return TreatmentPlan
 	 */
-	
 
 	@Override
 	public TreatmentPlan getTreatmentPlan(PatientDetail patientDetail, int cost) {
-		log.info("START :: Method :: getTreatmentPlan() :: ");
 		Calendar currentDate = Calendar.getInstance();
 		String commencementDate = reviewDateAndMonth(currentDate.get(Calendar.DATE)) + "/"
 				+ reviewDateAndMonth(currentDate.get(Calendar.MONTH) + 1) + "/" + currentDate.get(Calendar.YEAR);
@@ -78,113 +72,93 @@ public class IPTreatmentServiceImpl implements IPTreatmentService {
 				patientDetail.getTreatmentPackageName(), specialist, cost, "In progress", commencementDate, endDate);
 
 		treatmentPlanRepository.save(plan);
-		log.info("END :: Method :: getTreatmentPlan() :: ");
 		return plan;
 	}
 
 	/**
-	 *  returns the reviewDatAndMonth
+	 * returns the reviewDatAndMonth
+	 * 
 	 * @param dateOrMonth
 	 * @return String
 	 */
-	
+
 	public String reviewDateAndMonth(int dateOrMonth) {
-		log.info("START :: Method :: reviewDateAndMonth() :: ");
-		log.info("END :: Method :: reviewDateAndMonth() :: ");
 		if (dateOrMonth < 10)
 			return "0" + Integer.toString(dateOrMonth);
 		else
 			return Integer.toString(dateOrMonth);
 	}
 
-
 	/**
 	 * to return all the treatment plan
 	 * 
 	 * @return List<TreatmentPlan>
 	 */
-	
-	
+
 	@Override
 	public List<TreatmentPlan> getAllPlans() {
-		log.info("START :: Method :: getAllPlans() :: ");
-		log.info("END :: Method :: getAllPlans() :: ");
 		return treatmentPlanRepository.getAllplans();
 	}
+
 	/**
 	 * to return all the Ongoing treatment plan
 	 * 
 	 * @return List<TreatmentPlan>
 	 */
-	
-	
+
 	@Override
 	public List<TreatmentPlan> getAllOnGoingplans() {
-		log.info("START :: Method :: getAllOnGoingplans() :: ");
-		log.info("END :: Method :: getAllOnGoingplans() :: ");
 		return treatmentPlanRepository.getAllOnGoingplans();
 	}
 
-	
 	/**
 	 * to return the specific patient details
+	 * 
 	 * @param id
 	 * @return PatientDetail
 	 */
-	
-	
+
 	@Override
 	public PatientDetail getPatient(long ptId) {
-		log.info("START :: Method :: getPatient() :: ");
-		log.info("END :: Method :: getPatient() :: ");
 		return patientDetailRepository.getPatientById(ptId);
 	}
 
-	
-
 	/**
 	 * to return the specific treatment plan
+	 * 
 	 * @param id
 	 * @return TreatmentPlan
 	 */
-	
+
 	@Override
 	public TreatmentPlan getPlan(long ptId) {
-		log.info("START :: Method :: getPlan() :: ");
-		log.info("END :: Method :: getPlan() :: ");
 		return treatmentPlanRepository.getPlanUser(ptId);
 	}
 
-	
 	/**
 	 * to update the treatment plan
+	 * 
 	 * @param id
 	 */
-	
+
 	@Override
 	public void updatePlan(long pId) {
-		log.info("START :: Method :: updatePlan() :: ");
 		TreatmentPlan tp = treatmentPlanRepository.getPlanUser(pId);
 		tp.setStatus("Completed");
 		treatmentPlanRepository.save(tp);
-		log.info("END :: Method :: updatePlan() :: ");
 	}
-
-	
 
 	/**
 	 * to return all the registered patient
 	 * 
 	 * @return List<PatientDetail>
 	 */
-	
+
 	@Override
 	public List<PatientDetail> getAllPatients() {
-		log.info("START :: Method :: getAllPatients() :: ");
-		log.info("END :: Method :: getAllPatients() :: ");
 		return patientDetailRepository.findAll();
 	}
-	
+
 	public AuthResponse validateToken(String token) {
 
 		AuthResponse authResponse = null;
